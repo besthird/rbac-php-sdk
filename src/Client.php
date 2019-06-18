c61a8465fcc9a615e9c3bb7cb02505c4f89c14ce
李铭昕 <715557344@qq.com>
2019-06-18 15:18:05 +0800

    初始化

---
+++ src/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of besthrid.
+ *
+ * @contact  l@hyperf.io
+ */
+
+namespace Besthird\RbacSdk;
+
+class Client
+{
+}
---
+++ tests/Cases/AbstractTestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of besthrid.
+ *
+ * @contact  l@hyperf.io
+ */
+
+namespace Test\Cases;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTestCase.
+ */
+abstract class AbstractTestCase extends TestCase
+{
+}
