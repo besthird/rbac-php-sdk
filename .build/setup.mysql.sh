741748b95f0c8bcb078a1ab5d2a989fe597af921
李铭昕 <715557344@qq.com>
2019-07-03 15:16:27 +0800

    Update

--- .build/setup.mysql.sh
+++ .build/setup.mysql.sh
@@ -4,8 +4,8 @@ CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
 
 echo -e "Create MySQL database..."
-mysql -u root -P 13306 -e "CREATE DATABASE IF NOT EXISTS rbac charset=utf8mb4 collate=utf8mb4_unicode_ci;"
-cat "${TRAVIS_BUILD_DIR}/.build/rbac.sql" | mysql -u root rbac -P 13306
+mysql -h 127.0.0.1 -u root -P 13306 -e "CREATE DATABASE IF NOT EXISTS rbac charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+cat "${TRAVIS_BUILD_DIR}/.build/rbac.sql" | mysql -h 127.0.0.1 -u root rbac -P 13306
 echo -e "Done\n"
 
 wait
--- tests/Cases/AuthTest.php
+++ tests/Cases/AuthTest.php
@@ -23,8 +23,6 @@ class AuthTest extends AbstractTestCase
 
         $res = $client->auth->check('admin', 1, 'GET', '/');
 
-        var_dump($res);
-        
         $this->assertSame(0, $res[0]);
     }
 
